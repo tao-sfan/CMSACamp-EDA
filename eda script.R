1ae0d1dacd0e245a8298d0466c9c2e8c039f4ddd
gracefain <gfain@ou.edu>
2021-06-08 15:21:20 -0500

    eda script, match length, RvL hand, dpSaved

---
+++ eda script.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+wta_2018_2021_matches <-
+  map_dfr(c(2018:2021),
+          function(year) {
+            read_csv(paste0("https://raw.githubusercontent.com/JeffSackmann/tennis_wta/master/wta_matches_",
+                            year, ".csv")) %>%
+              mutate(winner_seed = as.character(winner_seed),
+                     loser_seed = as.character(loser_seed))
+          })
+
+head(wta_2018_2021_matches)
+
+# minutes for each round ----
+table(wta_2018_2021_matches$round)
+
+wta_2018_2021_matches %>%
+  ggplot(aes(x = round)) +
+  geom_point(aes(y = minutes))
+
+# right and left hand ----
+wta_2018_2021_matches %>%
+  filter(winner_hand %in% c("R", "L"), loser_hand %in% c("R", "L")) %>%
+  group_by(winner_hand, loser_hand) %>%
+  summarise(n())
+
+# prop of bp saved for winners v losers ----
+wta_2018_2021_matches %>%
+  count(w_bpSaved/w_bpFaced, l_bpSaved/l_bpFaced)
+
+# number of wins each player has
+wta_2018_2021_matches %>%
+  count(winner_id) %>%
+  arrange(desc(n()))
+
+   
+
+
